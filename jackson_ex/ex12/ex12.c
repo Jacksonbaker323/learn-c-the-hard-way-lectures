4305eba2063ed04032063736cfb445dadb7a5b60
Jacksonbaker323 <Jacksonbaker323@Gmail.com>
2026-04-06 19:13:56 -0700

    ex12

---
+++ jackson_ex/ex12/ex12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int areas[] = {10, 12, 13, 14, 20};
+
+  char name[] = "Zed";
+  char full_name[] = {'Z', 'e', 'd', ' ', 'A', '.', ' ', 'S', 'h', 'a', 'w', '\0''};
+
+  //WARNING on some systems you may hjave to change the %ld to use %u since it will use unsigned ints
+
+  printf("The size of an int: %ld\n")
+}
